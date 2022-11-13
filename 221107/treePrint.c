2990241033b6ce42a6778f836428b954123bf28a
cheomji <104716784+cheomji@users.noreply.github.com>
2022-11-13 16:14:42 +0900

    Update treePrint.c

--- 221107/treePrint.c
+++ 221107/treePrint.c
@@ -1,4 +1,79 @@
 // 큐 구현을 잘못해서 한참 헤맸다 .... ㅂㅅ
+
+/*
+이거는 트리 구현하는 부분 실습때 너무 얼렁뚱땅 한 것 같아서 숫자 ver. 으로 다ㅅㅣ 해본 거
+그뭐냐 큐는 n을 뭐 얼마를 넣든 꽉 차면 차지 절대 비지는 않으니까 큐가 비었을 때를 탈출조건으로 넣지  ㅇㅇ
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+typedef struct Node* treePointer;
+typedef struct Node{
+    int data;
+    treePointer leftChild, rightChild;
+}node;
+treePointer queue[MAX];
+int front, rear;
+
+treePointer createTree(int n);
+void addq(treePointer element);
+treePointer deleteq(void);
+void createNode(treePointer *cur, int n);
+void inorder(treePointer tree);
+
+int main(int argc, const char * argv[]) {
+    int n;
+    treePointer root;
+    scanf("%d", &n);
+    root = createTree(n);
+    inorder(root);
+    return 0;
+}
+treePointer createTree(int n){
+    treePointer root, tmp, cur;
+    int i = 1;
+    createNode(&root, i);
+    addq(root);
+    while (1) {
+        tmp = deleteq();
+        if (n < ++i) break;
+        createNode(&cur, i);
+        addq(cur);
+        tmp->leftChild = cur;
+        if (n < ++i) break;
+        createNode(&cur, i);
+        addq(cur);
+        tmp->rightChild = cur;
+    }
+    return root;
+}
+void addq(treePointer element){
+    if (rear == MAX-1){
+        printf("Queue full");
+        exit(EXIT_FAILURE);
+    }
+    queue[++rear] = element;
+}
+treePointer deleteq(){
+    if (front == rear){
+        printf("Queue empty");
+        return NULL;
+    }
+    return queue[++front];
+}
+void createNode(treePointer *cur, int n){
+    *cur = (treePointer)malloc(sizeof(node));
+    (*cur)->data = n;
+    (*cur)->leftChild = NULL;
+    (*cur)->rightChild = NULL;
+}
+void inorder(treePointer tree){
+    if (tree){
+        inorder(tree->leftChild);
+        printf("%d ", tree->data);
+        inorder(tree->rightChild);
+    }
+}
+*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
